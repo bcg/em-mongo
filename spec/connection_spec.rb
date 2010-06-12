e56e13b093da96b603cfe66bc35e94a138610358
Jari Bakken <jari.bakken@gmail.com>
2010-06-12 21:52:17 +0200

    Fix typo + clean up specs a little.

--- spec/connection_spec.rb
+++ spec/connection_spec.rb
@@ -7,37 +7,41 @@ describe EMMongo::Connection do
     em do
       connection = EMMongo::Connection.new
       EM.next_tick do
-        connection.connected?.should == true
+        connection.should be_connected
         done
       end
-    end    
+    end
   end
-  
+
   it 'should close' do
     em do
       connection = EMMongo::Connection.new
+
       EM.add_timer(1) do
-        connection.connected?.should == true
+        connection.should be_connected
         connection.close
       end
+
       EM.add_timer(2) do
         EM.next_tick do
-          connection.connected?.should == false
+          connection.should_not be_connected
           done
         end
       end
-    end 
+    end
   end
 
-  it 'should instantiate a Databse' do
+  it 'should instantiate a Database' do
     EM::Spec::Mongo.connection do |connection|
       db1 = connection.db
-      db1.is_a?(EM::Mongo::Database).should == true
+      db1.should be_kind_of(EM::Mongo::Database)
+
       db2 = connection.db('db2')
-      db2.is_a?(EM::Mongo::Database).should == true
+      db2.should be_kind_of(EM::Mongo::Database)
       db2.should_not == db1
+
       EM::Spec::Mongo.close
-    end 
+    end
   end
 
 
