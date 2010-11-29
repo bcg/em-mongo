03c984526e967d12a93a27ee4cf469dcee5caa05
Brenden Grace <brenden.grace@gmail.com>
2010-11-28 20:47:26 -0500

    Add reconnect_in instead of auto reconnecting.

---
+++ spec/integration/connection_spec.rb
@@ -0,0 +1,56 @@
+require File.expand_path('spec_helper', File.dirname(__FILE__) + '/../')
+
+describe EMMongo::Connection do
+  include EM::Spec
+  include EM::Mongo
+
+  it 'should connect' do
+    @conn = EMMongo::Connection.new
+    EM.add_timer(0.1) do
+      @conn.should be_connected
+      done
+    end
+  end
+
+  it 'should close' do
+    @conn = EMMongo::Connection.new
+
+    EM.add_timer(0.1) do
+      @conn.should be_connected
+      @conn.close
+    end
+
+    EM.add_timer(0.2) do
+      EM.next_tick do
+        @conn.should_not be_connected
+        done
+      end
+    end
+  end
+
+  it 'should reconnect' do
+    @conn = EMMongo::Connection.new(DEFAULT_IP, DEFAULT_PORT, nil, {:reconnect_in => 0.5})
+    EM.add_timer(0.1) do
+      @conn.close
+    end
+    EM.add_timer(0.9) do
+      @conn.should be_connected
+      done
+    end
+  end
+
+  it 'should instantiate a Database' do
+    @conn = EMMongo::Connection.new
+
+    db1 = @conn.db
+    db1.should be_kind_of(EM::Mongo::Database)
+
+    db2 = @conn.db('db2')
+    db2.should be_kind_of(EM::Mongo::Database)
+    db2.should_not == db1
+
+    done
+  end
+
+
+end
