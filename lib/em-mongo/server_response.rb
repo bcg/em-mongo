26875fba71e19b5b6dba0338abe8a5a832c94dfb
Christoph Sturm <me@christophsturm.com>
2011-09-13 03:30:54 +0200

    try to make it work without cbson

--- lib/em-mongo/server_response.rb
+++ lib/em-mongo/server_response.rb
@@ -2,35 +2,62 @@ module EM::Mongo
   class ServerResponse
 
     attr_reader :size, :request_id, :response_to, :op,
-      :result_flags, :cursor_id, :starting_from,
-      :number_returned, :docs, :connection
+                :result_flags, :cursor_id, :starting_from,
+                :number_returned, :docs, :connection
 
+    if defined? CBson
+      def initialize(buffer, connection)
+        @connection = connection
+        # Header
+        @size        = buffer.get_int
+        @request_id  = buffer.get_int
+        @response_to = buffer.get_int
+        @op          = buffer.get_int
 
-    def initialize(buffer, connection)
-      @connection = connection
-      # Header
-      @size        = buffer.get_int
-      @request_id  = buffer.get_int
-      @response_to = buffer.get_int
-      @op          = buffer.get_int
+        # Response Header
+        @result_flags     = buffer.get_int
+        @cursor_id        = buffer.get_long
+        @starting_from    = buffer.get_int
+        @number_returned  = buffer.get_int
 
-      # Response Header
-      @result_flags     = buffer.get_int
-      @cursor_id        = buffer.get_long
-      @starting_from    = buffer.get_int
-      @number_returned  = buffer.get_int
+        # Documents
+        pos=buffer.position
+        @docs = (1..@number_returned).map do
+          size= @connection.peek_size(buffer)
 
-      # Documents
-      pos=buffer.position
-      @docs = (1..@number_returned).map do
-        size= @connection.peek_size(buffer)
-
-        doc=CBson.deserialize(buffer.to_s[pos,size])
-        pos+=size
-        buffer.position=pos
-        doc
+          doc=CBson.deserialize(buffer.to_s[pos,size])
+          pos+=size
+          buffer.position=pos
+          doc
+        end
       end
-    end
+    else
+      def initialize(buffer, connection)
+        @connection = connection
+        # Header
+        @size        = buffer.get_int
+        @request_id  = buffer.get_int
+        @response_to = buffer.get_int
+        @op          = buffer.get_int
 
+        # Response Header
+        @result_flags     = buffer.get_int
+        @cursor_id        = buffer.get_long
+        @starting_from    = buffer.get_int
+        @number_returned  = buffer.get_int
+
+        # Documents
+        pos=buffer.position
+        @docs = (1..@number_returned).map do
+          size= @connection.peek_size(buffer)
+
+          doc=BSON::BSON_CODER.deserialize(buffer.to_s[pos,size])
+          pos+=size
+          buffer.position=pos
+          doc
+        end
+      end
+
+    end
   end
 end
